140b6c69710adf4d50b605b52c36fc5e5b0a7eea
pui <hockyangui@gmail.com>
2025-02-22 21:55:49 +0800

    use pdo to rewrite the program (assgnment need) dammmmmmmmmm

--- db.php
+++ db.php
@@ -1,8 +1,7 @@
-
 <?php 
 // make a class database connection for use 
 class Database {
-    // define the attritube that is fixed 
+    // define the attribute that is fixed 
     private $servername = "localhost";
     private $username = "root";
     private $password = "";
@@ -11,17 +10,26 @@ class Database {
 
     // __construct function : special function that gets automatically executed when an object of a class is created. 
     private function __construct(){
-        // the connection
-        // $this->variable is like object.variable in python 
-        // in here $this will be the class name : "Database" 
-        // $this->servername : Database servename : localhost   
-        $this->conn = new mysqli($this->servername , $this->username , $this->password, $this->dbname);
-        // check connection sucess or not 
-        if($this->conn->connect_error){
-            die("connection failed :".$this->conn->connect_error);
+        try {
+            // the connection
+            // $this->variable is like object.variable in python 
+            // in here $this will be the class name : "Database" 
+            // $this->servername : Database servername : localhost   
+            $dsn = "mysql:host={$this->servername};dbname={$this->dbname};charset=utf8";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+
+            // set PDO attributes 
+            // PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ensures that errors are thrown as exceptions
+            // PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC makes sure fetched data is returned as an associative array
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage()); // die() function to stop script execution if connection fails
         }
     }
-    // fucntion to create and get the connection 
+
+    // function to create and get the connection 
     public static function getConnection(){
         static $instance = null ;
         if($instance === null ){
@@ -29,19 +37,18 @@ class Database {
         }
         return $instance->conn;
     }
+
     // function to close connection :
-    // if the connection is exist than close it 
+    // if the connection exists then close it 
     public function closeConnection(){
-        if($this->conn !== null ){
-            $this->conn->close();
-            $this->conn = null ;
-        }
+        $this->conn = null; // PDO automatically closes the connection when the object is destroyed
     }
 }
+
 // how to use :
-// 1. go to the file you want to connect with databse 
-// 2. apply this file into it : require_once 'db.php'; depends where you import you need to consider use ../ absulote locate 
+// 1. go to the file you want to connect with the database 
+// 2. apply this file into it : require_once 'db.php'; depends where you import, you need to consider using ../ absolute path 
 // 3. create a connection variable : $conn = Database::getConnection();
-// 4. check it scuess or not : echo $conn ? "db Connected" : "Failed to connect";
-// 5. at the end of the program you can $conn->closeConnection to close the link manually or you can let it closed auto 
+// 4. check if it succeeds or not : echo $conn ? "db Connected" : "Failed to connect";
+// 5. at the end of the program you can call $conn->closeConnection() to close the link manually or let it close automatically 
 ?>
--- db_test.php
+++ db_test.php
@@ -8,11 +8,16 @@ class testDb {
     private $password = "";
 
     // fucntion to make a object of database and connect it auto
-    public function __construct($dbName){
-        $this->conn = new mysqli($this->servername , $this->username , $this->password , $dbName);
-        // check connection
-        if($this->conn->connect_error){
-            die("connection failed".$this->conn->connect_error);
+    public function __construct($dbName) {
+        try {
+            $dsn = "mysql:host={$this->servername};dbname={$dbName};charset=utf8";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            // 设置 PDO 错误模式为异常模式
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // 设置数据返回方式为关联数组
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die("数据库连接失败: " . $e->getMessage());
         }
     }
     // fucntion to get the connection 
@@ -22,15 +27,15 @@ class testDb {
     // function to close the connection manually 
     public function closeConnection(){
         if($this->conn !== null ){
-            $this->conn->close();
             $this->conn = null ;
         }
     }
 }
+// how to use : 
 // 1. go to the file you want to connect with databse 
 // 2. apply this file into it : require_once 'db_test.php'; depends where you import you need to consider use ../ absulote locate 
-// 3. create a database object : $db = new Database(pass the variable inside: "yourtestdbname")
+// 3. create a database object : $db = new testDb(pass the variable inside: "yourtestdbname")
 // 4. use the object function to make connection variable : $db->getConnection()
 // 5. check it scuess or not : echo $conn ? "db Connected" : "Failed to connect";
-// 6. at the end of the program you can $conn->closeConnection to close the link manually or you can let it closed auto 
+// 6. at the end of the program you can $db->closeConnection(); to close the link manually or you can let it closed auto 
 ?>
