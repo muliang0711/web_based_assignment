1496f0976899a25c69d04625a3ab57b23f4baff0
pui <hockyangui@gmail.com>
2025-02-22 21:29:06 +0800

    fix bug

--- db.php
+++ db.php
@@ -41,7 +41,7 @@ class Database {
 // how to use :
 // 1. go to the file you want to connect with databse 
 // 2. apply this file into it : require_once 'db.php'; depends where you import you need to consider use ../ absulote locate 
-// 3. create a connection variable : $conn = Databse::getConnection();
+// 3. create a connection variable : $conn = Database::getConnection();
 // 4. check it scuess or not : echo $conn ? "db Connected" : "Failed to connect";
 // 5. at the end of the program you can $conn->closeConnection to close the link manually or you can let it closed auto 
 ?>
