8a82759787a39e1c656e33fabb458167a1a56705
pui <hockyangui@gmail.com>
2025-02-24 18:43:47 +0800

    make it more clean and explain

--- mvc_demo/index.php
+++ mvc_demo/index.php
@@ -1,15 +1,6 @@
 <?php
 require_once __DIR__ . '/controller/product_controller.php';
-
+require_once __DIR__ . '/routers/web.php';
 // Get the requested URL
-$url = isset($_GET['url']) ? $_GET['url'] : '';
-
-$controller = new ProductController();
-
-if ($url == 'product/delete' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    $controller->deleteProduct();
-} else {
-    $controller->listProducts(); // Default: Show product list
-}
 
 ?>
