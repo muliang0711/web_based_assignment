0d3e35985c0b9cce2e05949af84f9d57cb7d7dff
Wayne24 <wayneganyuwei@gmail.com>
2025-02-25 23:40:06 +0800

    updated order.php

---
+++ pages/order/order.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=`device-width`, initial-scale=1.0">
+    <title>Order | Page</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="order.css">
+    <link rel="icon" type="image/svg+xml" href="../../assets/img/shuttlecock.svg">
+</head>
+<body>
+    <h1>ORDER PAGE COMING SOON</h1>
+</body>
+</html>
