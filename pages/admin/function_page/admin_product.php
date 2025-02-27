bb1d430d104fca0035736bf46446e1b2acf33b0e
du-52 <songlynn12@gmail.com>
2025-02-27 15:16:07 +0800

    adminfolder
    。

---
+++ pages/admin/function_page/admin_product.php
@@ -0,0 +1,17 @@
+<?php
+
+$_title='Product';
+include '../admin_head.php';
+?>
+
+<?php
+for ($i = 1; $i <= 3000; $i++) {
+    echo "<div>$i time(s)</div>";
+}
+?>
+
+
+<?php 
+include '../admin_foot.php';
+?>
+
--- pages/admin/header_footer/admin_head.php
+++ pages/admin/header_footer/admin_head.php
@@ -4,21 +4,21 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home Page</title>
-    <link rel="stylesheet" href="display.css">
+    <link rel="stylesheet" href="../display.css">
 
 </head>
 
 <body>
     <div class="side-container">
         <p class="display_topleft adminHeader"><b><i>Admin</i></b></p>
-        <a onclick="loadIframe('/pages/product/product.html')" class="button">Home</a>
-        <a onclick="loadIframe('/pages/product/productlist.html')" class="button">Order</a>
+        <a href="/pages/admin/function_page/admin_product.php" class="button">Home</a>
+        <a href="/pages/product/productlist.php" class="button">Order</a>
         <a class="button">Product</a>
         <a class="button">Customer</a>
         <a class="button">Discount/Voucher</a>
         <a class="button">Analysis</a>
         <a class="display_bottomleft">
-            <img src="admin_image/signout.jpg" width="20px">
+            <img src="../admin_image/signout.jpg" width="20px">
         </a>
     </div>
 
