9e9dba6dfec9fe50931e96e809cdb76b6f1eef2e
Wayne24 <wayneganyuwei@gmail.com>
2025-02-28 00:49:16 +0800

    sike just one last one

--- pages/order/order.js
+++ pages/order/order.js
@@ -2,6 +2,11 @@ $(() => {
 
 
     $(".dropdown").on('click', function() {
+
+        $(".rotate").not($(this)).removeClass("rotate");
+        $(".show").not($(this).closest(".order").next("div")).removeClass("show");
+        $(".noanimation").not($(this).closest(".order")).removeClass("noanimation");
+
         $(this).toggleClass('rotate');
         $(this).closest(".order").next("div").toggleClass("show");
         $(this).closest(".order").toggleClass("noanimation");
