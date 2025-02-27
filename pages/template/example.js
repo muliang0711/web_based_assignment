408995daffbc3cf18bf3b6e3fad3c3c1cbd7a524
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-02-27 16:23:07 +0800

    Minor: comment in example.js

--- pages/template/example.js
+++ pages/template/example.js
@@ -1,6 +1,9 @@
 $(() => {
 
+    // Select any <h1> elements, hide them first, then fade in
     $('h1').hide().fadeIn();
+
+    // Select any <p> elements, hide them first, then fade in for 1 second
     $('p').hide().fadeIn(1000);
 
 })
