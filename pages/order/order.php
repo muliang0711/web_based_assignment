3affb6cc86db0f85d47486f608c18b2ab1268970
Wayne24 <wayneganyuwei@gmail.com>
2025-02-27 23:04:09 +0800

    updated order page

--- pages/order/order.php
+++ pages/order/order.php
@@ -2,14 +2,27 @@
     require "../../_base.php";
     $title = 'Order | Page';
     $time = time();
-    $stylesheetArray  = ["order.css?v={$time}"];
+    $stylesheetArray  = ["order.css?"];
     include "../../_head.php";
+
+    $orders = [
+
+    ];
 ?>
 
 
 <h1>My orders</h1>
 <div class="orders-container">
-    <div class="order"></div>
+    <div class="order-header">
+        <span>Order ID</span>
+        <span>Address</span>
+        <span>Ordered Date</span>
+    </div>
+    <div class="order">
+        <span class="orderID">#12345</span>
+        <span class="orderAddress">PV18 RESIDENCES Kuala  Jalan Langkawi LUMPUR</span>
+        <span class="orderDate">27/2/2025</span>
+    </div>
     <div class="order"></div>
     <div class="order"></div>
 </div>
