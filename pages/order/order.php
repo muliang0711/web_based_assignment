3f3543f8e6c678062188dd1d26e52b8c118e72cd
Wayne24 <wayneganyuwei@gmail.com>
2025-02-28 00:30:38 +0800

    one final commit before i stick to my bed

--- pages/order/order.php
+++ pages/order/order.php
@@ -29,8 +29,26 @@
         </button>
     </div>
     <div class="orderDetailed"></div>
-    <div class="order"></div>
-    <div class="order"></div>
+    <div class="order">
+        <span class="orderID">#55125</span>
+        <span class="orderAddress">jalan ujong Pasir melaka 707070 malaysia</span>
+        <span class="orderDate">12/8/2025</span>
+        <span class="orderStatus">Delivered</span>
+        <button class="dropdown">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg>
+        </button>
+    </div>
+    <div class="orderDetailed"></div>
+    <div class="order">
+        <span class="orderID">#66125</span>
+        <span class="orderAddress">PV18 RESIDENCES Kuala  Jalan Langkawi LUMPUR</span>
+        <span class="orderDate">27/2/2025</span>
+        <span class="orderStatus">In Transit</span>
+        <button class="dropdown">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z"/></svg>
+        </button>
+    </div>
+    <div class="orderDetailed"></div>
 </div>
 
 <?php 
