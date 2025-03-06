a02e5f76c70a9f72f7da537029a0280893b32ba8
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-03-06 19:21:02 +0800

    Move file

--- pages/admin/adminAdd.php
+++ pages/admin/adminAdd.php
@@ -3,7 +3,7 @@ require '../../_base.php';
 
 $title='Admin Management';
 $stylesheetArray = ['/css/admin_management.css'];   // 注意：这边只放特定于此页面的 .css file(s)。所有 admin 页面都会用到的 .css files 应放在 /css/admin.css
-$scriptArray = ['../../base.js'];       // 注意：这边只放特定于此页面的 .js file(s)。所有 admin 页面都会用到的 .js files 应放在 /js/admin.js
+$scriptArray = ['/js/app.js'];       // 注意：这边只放特定于此页面的 .js file(s)。所有 admin 页面都会用到的 .js files 应放在 /js/admin.js
 
 include '../../admin_head.php';
 ?>
