cdde8e5c3bc5b4ff1b31f35f964ce79df490e683
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-03-14 00:07:53 +0800

    fix product css

--- pages/product/productDetail.php
+++ pages/product/productDetail.php
@@ -3,7 +3,7 @@
 </html>
 <link rel="stylesheet" href="./product/productlist.php">
 <?php
-$stylesheetArray = ['product.css'];
+$stylesheetArray = ['product-temp.css'];
 
 function link_stylesheet($stylesheetArray) {
   if (!$stylesheetArray) {
