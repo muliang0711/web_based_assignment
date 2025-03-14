1f73780842da35ec996191363094fc4d5a4cbe2a
Kai7314 <kaikaikai7314@gmail.com>
2025-03-14 23:15:27 +0800

    Add search bar in product list page

--- pages/product/searchResult.php
+++ pages/product/searchResult.php
@@ -1,37 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-</html>
-
 <?php
-$stylesheetArray = ['product.css'];
+require '../../_base.php';
 
-function link_stylesheet($stylesheetArray) {
-  if (!$stylesheetArray) {
-      return;
-  }
-}
-  $time = time();
-  
-  if (is_array($stylesheetArray)) {
-      foreach ($stylesheetArray as $stylesheet) {
-          echo "<link rel='stylesheet' href='$stylesheet?v=$time' />";
-      }
-  } 
+$stylesheetArray = ['product.css'];
+$title = 'Product List';
 ?>
 
 <body>
-    <form method="get">
-    <input type="text" id="search" name="search" maxlength="30">
-    <button>submit</button>
-    </form>
+<form method="get" action="../product/searchResult.php?search=?">
+    <div class="searchContainer">
+     <input type="text" id="search" name="search" maxlength="30" class="input" placeholder="S E A R C H">
+    </div>
+   <button><img src="illustration-magnifying-glass-icon.png"></button>
+</form> 
     <?php
-        function is_get() {
-        return $_SERVER['REQUEST_METHOD'] == 'GET';
-        }
         $search = $_REQUEST['search'];
         if(!$search){
             echo "Invalid input!";
-            redirect("../product/productlist.php");
         }
         $_db = new PDO('mysql:dbname=web_based_assignment','root', '',[
                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
