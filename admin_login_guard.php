48c55ae13f79beee276fc306e48ca01b9337547f
Cookiecodess <142579195+Cookiecodess@users.noreply.github.com>
2025-03-18 20:10:24 +0800

    package login-guard code into a php file

---
+++ admin_login_guard.php
@@ -0,0 +1,6 @@
+<?php
+if (!is_logged_in()) {
+    temp('info', 'You must log in first.');
+    temp('fromPage', $_SERVER['REQUEST_URI']); // this ensures that after user logs in, they'll be redirected back to this page. 
+    redirect('/pages/admin/admin-login.php');
+}
--- pages/user/profile.php
+++ pages/user/profile.php
@@ -6,11 +6,7 @@ $title = 'Profile';
 $stylesheetArray = ['profile.css']; // Put CSS files that are specific to this page here. If you want to change the styling of the header and the footer, go to /css/app.cs
 $scriptArray = [];      // Put JS files that are specific to this page here. If you want to change the JavaScript for the header and the footer, go to /js/app.js
 
-if (!is_logged_in()) {
-    temp('info', 'You must log in first.');
-    temp('fromPage', $_SERVER['REQUEST_URI']); // this ensures that after user logs in, they'll be redirected back to this page. 
-    redirect('/pages/user/user-login.php');
-}
+include '../../_login_guard.php';
 
 include '../../_head.php';
 ?>
