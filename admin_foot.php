6b0d1326739dea22e004c076a745b53ef6514326
pui <hockyangui@gmail.com>
2025-03-24 22:46:42 +0800

    add jquery import on foot

--- admin_foot.php
+++ admin_foot.php
@@ -2,6 +2,7 @@
     <!-- </div> -->
     </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="/js/admin.js"></script>
 <?= link_script($scriptArray ?? ''); ?>
 </html>
