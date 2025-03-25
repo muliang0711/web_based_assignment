49b4a04e23077e0122477906c0a9f080b0b7d53f
du-52 <songlynn12@gmail.com>
2025-03-25 21:48:53 +0800

    js

---
+++ js/admin.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $(".add").onclick(function(){
+  ;
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".show").click(function(){
+      $("orderDetailed").hide(1000);
+    });
+    $(".dropdownlist").click(function(){
+      $("orderDetailed").show(1000);
+    });
+  });
+  
