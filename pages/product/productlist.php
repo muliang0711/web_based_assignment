0ad528fc79d10acd3ae705861a590372d9094119
Kai7314 <kaikaikai7314@gmail.com>
2025-03-30 01:08:11 +0800

    img change to png

--- pages/product/productlist.php
+++ pages/product/productlist.php
@@ -19,7 +19,7 @@ $title = 'Product List';
 
 // Get product data from database
 $_db = new PDO('mysql:dbname=web_based_assignment', 'root', '', [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
 ]);
 $statement = $_db->prepare("SELECT * FROM product");
 $statement->execute([]);
@@ -38,43 +38,43 @@ if (is_logged_in("user")) {
 include '../../_head.php';
 ?>
 <div class="sidebar">
-    <div class="sidebarFont">
-        <ul>
-            <h>Series</h>
-            <hr>
-            <?php foreach ($seriesArray as $s): ?>
-                <a onclick="onclick()" href ="../product/searchResult.php?search=<?php echo $s->seriesName ?>" >
-                <p><?php echo "$s->seriesName" ?></p>
-                </a>
-            <?php endforeach ?>
-            <hr>
-            <h>Price Sorting</h>
-            <hr>
-            <form method="get" action="../product/searchResult.php?price=LowToHigh">
-            <p>Low  ->  High</p>
-            </form>
-            <form method="get" action="../product/searchResult.php?price=HighToLow">
-            <p>High ->  Low</p>
-            </form>
-            <hr>
-        </ul>
-    </div>
+  <div class="sidebarFont">
+    <ul>
+      <h>Series</h>
+      <hr>
+      <?php foreach ($seriesArray as $s): ?>
+        <a onclick="onclick()" href="../product/searchResult.php?search=<?php echo $s->seriesName ?>">
+          <p><?php echo "$s->seriesName" ?></p>
+        </a>
+      <?php endforeach ?>
+      <hr>
+      <h>Price Sorting</h>
+      <hr>
+      <form method="get" action="../product/searchResult.php?price=LowToHigh">
+        <p>Low -> High</p>
+      </form>
+      <form method="get" action="../product/searchResult.php?price=HighToLow">
+        <p>High -> Low</p>
+      </form>
+      <hr>
+    </ul>
+  </div>
 </div>
 
 
 <div class="main-container">
-    <form method="get" action="../product/searchResult.php?search=?">
-        <div class="searchContainer">
-            <input type="text" id="search" name="search" maxlength="30" class="input" placeholder="S E A R C H">
-        </div>
-        <div class="searchButton">
-            <button><img src="illustration-magnifying-glass-icon.png"></button>
-        </div>
-    </form>
-    
-    
-    <!-- TopSide Menu -->
-    <!-- <div class="menu">
+  <form method="get" action="../product/searchResult.php?search=?">
+    <div class="searchContainer">
+      <input type="text" id="search" name="search" maxlength="30" class="input" placeholder="S E A R C H">
+    </div>
+    <div class="searchButton">
+      <button><img src="illustration-magnifying-glass-icon.png"></button>
+    </div>
+  </form>
+
+
+  <!-- TopSide Menu -->
+  <!-- <div class="menu">
         <nav>
             <div class="top-sideMenu">
                 <ul>
@@ -95,71 +95,79 @@ include '../../_head.php';
             </div>
         </nav>
     </div> -->
-    
-    <!-- Nanoflare 1000z image -->
-    <div class="image-box">
-        <img src="aerosharp ad2.jpg" alt="Nanoflare 700 RISING">
-    </div>
-    
-    <hr>
-    
-    <!-- product Image -->
+
+  <!-- Nanoflare 1000z image -->
+  <div class="image-box">
+    <img src="aerosharp ad2.jpg" alt="Nanoflare 700 RISING">
+  </div>
+
+  <hr>
+
+  <!-- product Image -->
+  <?php
+  $_db = new PDO('mysql:dbname=web_based_assignment', 'root', '', [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+  ]);
+
+  $statement = $_db->prepare("SELECT * FROM product");
+  $statement->execute([]);
+  $productObjectArray = $statement->fetchAll();
+  $statementImg = $_db->prepare("SELECT * FROM product_images WHERE image_type = 'product'");
+  $statementImg->execute([]);
+  $productObjectArrayImg = $statementImg->fetchAll();
+  ?>
+
+  <div class="list" id="productList">
     <?php
-    $_db = new PDO('mysql:dbname=web_based_assignment', 'root', '', [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-    ]);
-    
-    $statement = $_db->prepare("SELECT * FROM product");
-    $statement->execute([]);
-    $productObjectArray = $statement->fetchAll();
-    
-    ?>
-    
-    <div class="list" id="productList">
-        <?php
-        foreach ($productObjectArray as $productObject): ?>
+  
+      foreach ($productObjectArray as $productObject): ?>
         <!-- start -->
         <div class="container">
         <!-- top side  -->
-            <div class="product-card">
-              <div class="top-side">
-                <div class="true-card">
-                  <div class="picture-card">
-                    <div class="picture">
-                      <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo $productObject->productImg; ?>" alt="Product Image" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-        <!-- middle side  -->
-              <div class="middle-side">
-                <div class="true-card">
-                  <div class="information">
-                    <div class="product-name dsc"><h2><?php echo $productObject->productName ?></h2></div>
-                    <div class="product-series-name dsc"><span><p>RM <?php echo $productObject->price ?>.00</p></span></div>
-                    <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
-                  </div>
-                </div>
-              </div>
-        <!-- bottom side  -->
-              <div class="bottom-side">
-                <div class="true-card">
-                  <div class="function">
-                    <div class="btn"><button class="btn">Add to Cart</button></div>
-                    <div class="btn"><button class="btn">Buy</button></div>
-                    <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
-                <!--<?php if(!$user){
-                    prompt_login("Please log in to add to cart.");}
-                    ?> -->
-                  </div>
+        <div class="product-card">
+          <div class="top-side">
+            <div class="true-card">
+              <div class="picture-card">
+                <div class="picture">
+                  <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo  $productObject->productImg; ?>" alt="Product Image" />
                 </div>
               </div>
             </div>
-        <!-- the end  -->
+          </div>
+          <!-- middle side  -->
+          <div class="middle-side">
+            <div class="true-card">
+              <div class="information">
+                <div class="product-name dsc">
+                  <h2><?php echo $productObject->productName ?></h2>
+                </div>
+                <div class="product-series-name dsc"><span>
+                    <p>RM <?php echo $productObject->price ?>.00</p>
+                  </span></div>
+                <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
+              </div>
+            </div>
+          </div>
+          <!-- bottom side  -->
+          <div class="bottom-side">
+            <div class="true-card">
+              <div class="function">
+                <div class="btn"><button class="btn">Add to Cart</button></div>
+                <div class="btn"><button class="btn">Buy</button></div>
+                <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
+                <!--<?php if (!$userID) {
+                      prompt_login("Please log in to add to cart.");
+                    }
+                    ?> -->
+              </div>
+            </div>
+          </div>
         </div>
+        <!-- the end  -->
+      </div>
 
-        <?php endforeach ?>
-    </div>
+    <?php endforeach ?>
+  </div>
 </div>
 
 <?php
