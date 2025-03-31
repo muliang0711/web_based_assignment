d0eecb2d0a48ca336d885abb76edbcb0b30dcf14
du-52 <songlynn12@gmail.com>
2025-04-01 00:11:13 +0800

    unblock request

---
+++ userRequestUnblock.php
@@ -0,0 +1,17 @@
+<?php
+require '../../_base.php';
+// ----------------------------------------------------------------------------
+
+if (is_post()) {
+    $userID=req('userID');
+    $stm = $_db->prepare('UPDATE user 
+    SET blockedStatus="request" 
+    WHERE userID = :userID
+');
+$stm->execute([
+    'userID' => $userID,
+    
+]);
+temp('info','You have request unblock to the admin');
+}
+redirect('view_customer.php');
