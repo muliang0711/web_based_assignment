f0c569d6e4bf3457aace67783dd784e17ea8547a
Kai7314 <kaikaikai7314@gmail.com>
2025-04-01 17:48:34 +0800

    add pagination update search bar

--- pages/product/searchResult.php
+++ pages/product/searchResult.php
@@ -11,146 +11,159 @@ $seriesArray = $seriesStatement->fetchAll();
 
 <body>
 
-<div class="sidebar">
+  <!-- Side bar -->
+  <div class="sidebar">
     <div class="sidebarFont">
-        <ul>
-            <h>Series</h>
-            <hr>
-            <?php foreach ($seriesArray as $s): ?>
-                <a onclick="onclick()" href ="../product/searchResult.php?search=<?php echo $s->seriesName ?>" >
-                <p><?php echo "$s->seriesName" ?></p>
-                </a>
-            <?php endforeach ?>
-            <hr>
-            <h>Price Sorting</h>
-            <hr>
-            <a onclick="onclick()" href="../product/searchResult.php?price=asc&search=<?php echo $search ?>">
-             <p>Low to High</p>
-            </a>
-           <a onclick="onclick()" href="../product/searchResult.php?price=desc&search=<?php echo $search ?>">
-            <p>High to Low</p>
-           </a>
-            <hr>
-        </ul>
+      <ul>
+        <h>Series</h>
+        <hr>
+        <?php foreach ($seriesArray as $s): ?>
+          <a onclick="onclick()" href="../product/searchResult.php?search=<?php echo $s->seriesName ?>">
+            <p><?php echo "$s->seriesName" ?></p>
+          </a>
+        <?php endforeach ?>
+        <hr>
+        <h>Price Sorting</h>
+        <hr>
+        <a onclick="onclick()" href="../product/searchResult.php?price=asc&search=<?php echo $search ?>">
+          <p>Low to High</p>
+        </a>
+        <a onclick="onclick()" href="../product/searchResult.php?price=desc&search=<?php echo $search ?>">
+          <p>High to Low</p>
+        </a>
+        <hr>
+      </ul>
     </div>
-</div>
+  </div>
 
-<div class="main-container">
+
+  <!-- Search bar -->
+  <div class="main-container">
     <form method="get" action="../product/searchResult.php?search=?">
-        <div class="searchContainer">
-            <input type="text" id="search" name="search" maxlength="30" class="input" placeholder="<?php echo $search ?>">
-        </div>
+      <div class="searchContainer">
+        <input type="text" id="search" name="search" maxlength="30" class="input" placeholder="<?php echo $search ?>">
         <div class="searchButton">
-            <button><img src="illustration-magnifying-glass-icon.png"></button>
+          <button><img src="illustration-magnifying-glass-icon.png"></button>
         </div>
+      </div>
     </form>
 
+
+    <!-- Default setting of sorting function -->
     <?php $order = isset($_GET['price']) && $_GET['price'] == 'desc' ? 'DESC' : 'ASC'; ?>
 
-    <?php
-    if (!$search) {
-        echo "Invalid input!";
-    }
-    $_db = new PDO('mysql:dbname=web_based_assignment', 'root', '', [
+
+
+
+<?php
+        $_db = new PDO('mysql:dbname=web_based_assignment', 'root', '', [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-    ]);
-    $statement = $_db->prepare("SELECT * FROM product JOIN product_images USING (productID) WHERE image_type = 'product' AND productName LIKE ? ORDER BY price $order");
-    $statement->execute(["%$search%"]);
-    $productObjectArray = $statement->fetchAll();
-    ?>
-    <?php if ($productObjectArray): ?>
-        <div class="list" id="productList">
+        ]);
+        $statement = $_db->prepare("SELECT * FROM product JOIN product_images USING (productID) WHERE image_type = 'product' AND productName LIKE ? ORDER BY price $order");
+        $statement->execute(["%$search%"]);
+        $productObjectArray = $statement->fetchAll();
+        ?>
+        <?php if ($productObjectArray): ?>
+          <div class="list" id="productList">
             <?php
- foreach ($productObjectArray as $productObject): ?>
-    <!-- start -->
-    <div class="container">
-    <!-- top side  -->
-        <div class="product-card">
-          <div class="top-side">
-            <div class="true-card">
-              <div class="picture-card">
-                <div class="picture">
-                <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo  $productObject->image_path; ?>" alt="Product Image" />
-                </div>
-              </div>
-            </div>
-          </div>
-    <!-- middle side  -->
-          <div class="middle-side">
-            <div class="true-card">
-              <div class="information">
-                <div class="product-name dsc"><h2><?php echo $productObject->productName ?></h2></div>
-                <div class="product-series-name dsc"><span><p>RM <?php echo $productObject->price ?>.00</p></span></div>
-                <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
-              </div>
-            </div>
-          </div>
-    <!-- bottom side  -->
-          <div class="bottom-side">
-            <div class="true-card">
-              <div class="function">
-                <div class="btn"><button class="btn">Add to Cart</button></div>
-                <div class="btn"><button class="btn">Buy</button></div>
-                <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
-              </div>
-            </div>
-          </div>
-        </div>
-    <!-- the end  -->
-    </div>
-            <?php endforeach ?>
-        </div>
-    <?php else: ?>
-        <div class="noResult">
-            <p>No result founded!</p>
-            <hr>
-            <div class="list" id="productList">
-                <?php
-                $statement = $_db->prepare("SELECT * FROM product");
-                $statement->execute([]);
-                $productObjectArray = $statement->fetchAll();
-                foreach ($productObjectArray as $productObject): ?>
-                            <!-- start -->
-        <div class="container">
-        <!-- top side  -->
-            <div class="product-card">
-              <div class="top-side">
-                <div class="true-card">
-                  <div class="picture-card">
-                    <div class="picture">
-                      <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo $productObject->productImg ?>" alt="Product Image" />
+            foreach ($productObjectArray as $productObject): ?>
+              <!-- start -->
+              <div class="container">
+                <!-- top side  -->
+                <div class="product-card">
+                  <div class="top-side">
+                    <div class="true-card">
+                      <div class="picture-card">
+                        <div class="picture">
+                          <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo  $productObject->image_path; ?>" alt="Product Image" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- middle side  -->
+                  <div class="middle-side">
+                    <div class="true-card">
+                      <div class="information">
+                        <div class="product-name dsc">
+                          <h2><?php echo $productObject->productName ?></h2>
+                        </div>
+                        <div class="product-series-name dsc"><span>
+                            <p>RM <?php echo $productObject->price ?>.00</p>
+                          </span></div>
+                        <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
+                      </div>
+                    </div>
+                  </div>
+                  <!-- bottom side  -->
+                  <div class="bottom-side">
+                    <div class="true-card">
+                      <div class="function">
+                        <!-- <div class="btn"><button class="btn">Add to Cart</button></div>
+                <div class="btn"><button class="btn">Buy</button></div> -->
+                        <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
+                      </div>
                     </div>
                   </div>
                 </div>
+                <!-- the end  -->
               </div>
-        <!-- middle side  -->
-              <div class="middle-side">
-                <div class="true-card">
-                  <div class="information">
-                    <div class="product-name dsc"><h2><?php echo $productObject->productName ?></h2></div>
-                    <div class="product-series-name dsc"><span><p>RM <?php echo $productObject->price ?>.00</p></span></div>
-                    <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
+            <?php endforeach ?>
+          </div>
+        <?php else: ?>
+          <div class="noResult">
+            <p>No result founded!</p>
+            <hr>
+            <div class="list" id="productList">
+              <?php
+              $statement = $_db->prepare("SELECT * FROM product JOIN product_images USING (productID) WHERE image_type = 'product' ORDER BY price $order");
+              $statement->execute([]);
+              $productObjectArray = $statement->fetchAll();
+              foreach ($productObjectArray as $productObject): ?>
+                <!-- start -->
+                <div class="container">
+                  <!-- top side  -->
+                  <div class="product-card">
+                    <div class="top-side">
+                      <div class="true-card">
+                        <div class="picture-card">
+                          <div class="picture">
+                            <img width="150px" height="250px" id="productImage" src="../../../File/<?php echo  $productObject->image_path ?>" alt="Product Image" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- middle side  -->
+                    <div class="middle-side">
+                      <div class="true-card">
+                        <div class="information">
+                          <div class="product-name dsc">
+                            <h2><?php echo $productObject->productName ?></h2>
+                          </div>
+                          <div class="product-series-name dsc"><span>
+                              <p>RM <?php echo $productObject->price ?>.00</p>
+                            </span></div>
+                          <div class="size-id dsc"><span>3UG5 / 4UG5</span></div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- bottom side  -->
+                    <div class="bottom-side">
+                      <div class="true-card">
+                        <div class="function">
+                          <div class="btn"><button class="btn">Add to Cart</button></div>
+                          <div class="btn"><button class="btn">Buy</button></div>
+                          <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
+                  <!-- the end  -->
                 </div>
-              </div>
-        <!-- bottom side  -->
-              <div class="bottom-side">
-                <div class="true-card">
-                  <div class="function">
-                    <div class="btn"><button class="btn">Add to Cart</button></div>
-                    <div class="btn"><button class="btn">Buy</button></div>
-                    <div class="btn"><button class="btn" onclick="window.location.href='../product/productDetail.php?racket=<?php echo $productObject->productID ?>'">View Details</button></div>
-                  </div>
-                </div>
-              </div>
+              <?php endforeach ?>
             </div>
-        <!-- the end  -->
+          </div>
+        <?php endif ?>
         </div>
-                <?php endforeach ?>
-            </div>
-        </div>
-    <?php endif ?>
-</div>
 </body>
 <?php
 include '../../_foot.php';
