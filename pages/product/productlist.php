ef9f3f8b91374e796bbd28fb4e2d16fd670c20a5
Kai7314 <kaikaikai7314@gmail.com>
2025-04-27 19:54:24 +0800

    db update

--- pages/product/productlist.php
+++ pages/product/productlist.php
@@ -133,7 +133,7 @@ include '../../_head.php';
         AND price BETWEEN $min_price AND $max_price 
         ORDER BY price $order",
         [],
-        "8", // Reminder: the `limit` parameter of the SimplePager constructor must be a string, e.g. "10". Can't pass an int due to the use of ctype_digit(). This behavior seems to be deliberate (look up the constructor definition), which makes it weirder. 
+        "6", // Reminder: the `limit` parameter of the SimplePager constructor must be a string, e.g. "10". Can't pass an int due to the use of ctype_digit(). This behavior seems to be deliberate (look up the constructor definition), which makes it weirder. 
         $page
       );
       $arr = $p->result;
